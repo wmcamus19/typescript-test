81c1ddba6736ed10d2398bba349d223a9b35b86f
Wilson M. Camus <wilsoncamus19@gmail.com>
2023-09-10 14:35:46 +0800

    Added math library.

---
+++ lesson-07/app/ts/script.ts
@@ -0,0 +1,13 @@
+let x: number = 4.432;
+let y: number = 2.123;
+let z: number = -43.453;
+let a: number = 234.2123123;
+
+console.log('floor():', Math.floor(x));
+console.log('ceil():', Math.ceil(x));
+console.log('abs():', Math.abs(z));
+console.log('pow():', Math.pow(x, 3));
+console.log('sqrt():', Math.sqrt(a));
+console.log('min():', Math.min(x, y, z, a));
+console.log('max():', Math.max(x, y, z, a));
+console.log('PI:', Math.PI);
---
+++ lesson-07/public/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+let x = 4.432;
+let y = 2.123;
+let z = -43.453;
+let a = 234.2123123;
+console.log('floor():', Math.floor(x));
+console.log('ceil():', Math.ceil(x));
+console.log('abs():', Math.abs(z));
+console.log('pow():', Math.pow(x, 3));
+console.log('sqrt():', Math.sqrt(a));
+console.log('min():', Math.min(x, y, z, a));
+console.log('max():', Math.max(x, y, z, a));
+console.log('PI:', Math.PI);
