ac8db4a282c0de0b3135bdd119eda8de95df5c0c
Wilson M. Camus <wilsoncamus19@gmail.com>
2023-09-10 14:22:44 +0800

    Added clog and alert.

---
+++ lesson-01/app/ts/script.ts
@@ -0,0 +1,6 @@
+console.log('Hello World');
+window.alert('Sauce!!');
+
+// One-line comment.
+/* Multi-line comment.
+I can do this. */
---
+++ lesson-01/public/js/script.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('Hello World');
+window.alert('Sauce!!');
+// One-line comment.
+/* Multi-line comment.
+I can do this. */
