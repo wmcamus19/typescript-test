13645cc703cbde75ffab188bdc28ab324ba6d9c3
Wilson M. Camus <wilsoncamus19@gmail.com>
2023-09-10 17:25:44 +0800

    Added method chaining.

---
+++ lesson-13/app/ts/script.ts
@@ -0,0 +1,5 @@
+let firstName: string =
+	'   Jojo Bizarre Adventures: Stardust Crusaders        ';
+
+console.log(firstName.trim());
+console.log(firstName.charAt(3).toUpperCase());
---
+++ lesson-13/public/js/script.js
@@ -0,0 +1,4 @@
+"use strict";
+let firstName = '   Jojo Bizarre Adventures: Stardust Crusaders        ';
+console.log(firstName.trim());
+console.log(firstName.charAt(3).toUpperCase());
