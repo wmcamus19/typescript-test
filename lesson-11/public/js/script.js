7bde5be1b1743731fb7073fb79d88f26e426f006
Wilson M. Camus <wilsoncamus19@gmail.com>
2023-09-10 14:52:41 +0800

    Added string methods.

---
+++ lesson-11/public/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let bookTitle = 'Jojo Bizarree Adventures: Stardust Crusaders.';
+console.log(bookTitle);
+console.log('Length:', bookTitle.length);
+console.log('toUpperCase():', bookTitle.toUpperCase());
+console.log('toLowerCase():', bookTitle.toLowerCase());
+console.log('charAt():', bookTitle.charAt(5));
+console.log('indexOf():', bookTitle.indexOf('B'));
+console.log('lastIndexOf():', bookTitle.lastIndexOf('.'));
+console.log('replace():', bookTitle.replaceAll('e', 'p'));
+bookTitle = '      Jojo Bizarree Adventures: Stardust Crusaders.             ';
+console.log('Non-trim:', bookTitle);
+console.log('trim():', bookTitle.trim());
+console.log('trimStart():', bookTitle.trimStart());
+console.log('trimeEnd():', bookTitle.trimEnd());
