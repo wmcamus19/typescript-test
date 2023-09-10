34890ba904e4524cd718ae2abf995b881f1940c0
Wilson M. Camus <wilsoncamus19@gmail.com>
2023-09-10 14:27:23 +0800

    Added arithmetic operations.

---
+++ lesson-03/app/ts/script.ts
@@ -0,0 +1,24 @@
+// Addition operator.
+let students: number = 30;
+students = students + 1;
+console.log('Addition:', students);
+
+// Subtraction operator.
+students = 30;
+students = students - 1;
+console.log('Subtraction:', students);
+
+// Multiplication operator.
+students = 30;
+students = students * 2;
+console.log('Multiplication:', students);
+
+// Division operator.
+students = 30;
+students = students / 2;
+console.log('Division:', students);
+
+// Modulos operator.
+students = 30;
+students %= 4;
+console.log('Modulos:', students);
